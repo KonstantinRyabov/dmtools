5d21a45bc1359e850e63b6d7656547980dbb299b
chachabooms <chachabooms@gmail.com>
2020-06-07 02:45:10 +0300

    fixed meddra_post

--- R/meddra.R
+++ R/meddra.R
@@ -12,6 +12,7 @@
 #' meddra_auth(url, id, key)
 #' }
 meddra_auth <- function(target_url, meddra_id, api_key) {
+  response_ok <- 200
 
   post_res <- httr::POST(
     url = target_url,
@@ -20,9 +21,12 @@ meddra_auth <- function(target_url, meddra_id, api_key) {
     encode = "form"
   )
 
+  if (post_res$status_code != response_ok) {
+    stop("response has status ", post_res$status_code)
+  }
+
   post_res <- httr::content(post_res)
-  token <- paste(post_res$token_type, post_res$access_token)
-  token
+  paste(post_res$token_type, post_res$access_token)
 }
 
 #' Create the post query
@@ -30,24 +34,27 @@ meddra_auth <- function(target_url, meddra_id, api_key) {
 #' @param target_url The url for a post query.
 #' @param json  A string scalar. The json query.
 #' @param token The user's token.
-#' @param encoding The json encoding. The default is utf-8.
 #'
 #' @return
-#' A data frame. The result of query.
+#' A list. The result of query.
 #' @export
 #'
 #' @examples
 #' \dontrun{
 #' meddra_post(url, json_body, token)
 #' }
-meddra_post <- function(target_url, json, token, encoding = "UTF-8") {
+meddra_post <- function(target_url, json, token) {
+  response_ok <- 200
 
   post_res <- httr::POST(
     url = target_url,
-    body = iconv(json, to = encoding),
-    httr::add_headers(Authorization = token), httr::accept_json(), httr::content_type_json()
+    body = json,
+    httr::add_headers(Authorization = token), httr::content_type_json()
   )
 
-  post_res <- httr::content(post_res)
-  tibble::tibble(result = post_res) %>% tidyr::unnest_wider(col = .data[["result"]])
+  if (post_res$status_code != response_ok) {
+    stop("response has status ", post_res$status_code)
+  }
+
+  httr::content(post_res)
 }
