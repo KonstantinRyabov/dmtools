9ac6b8eb181a3a6c75b49dfa0469a020337235f9
chachabooms <chachabooms@gmail.com>
2020-10-26 20:22:47 +0300

    fixed LBNRIND, docs and delete check sites

--- tests/testthat/test_lab.R
+++ tests/testthat/test_lab.R
@@ -3,7 +3,7 @@ site <- c("site 01", "site 02", "site 03")
 age <- c("19", "20", "22")
 sex <- c("f", "m", "f")
 gluc_post <- c(5.5, 4.1, 9.7)
-gluc_res_post <- c("norm", "no", "cl")
+gluc_res_post <- c("norm", "no", "no")
 ast_post <- c("30", "48", "31")
 ast_res_post <- c(NA, "norm", "norm")
 
@@ -14,23 +14,9 @@ df <- data.frame(
   stringsAsFactors = F
 )
 
-s01_lab <- lab("labs_refer.xlsx", id, age, sex, "norm", "no", clsig = "cl", site = "site 01")
-s02_lab <- lab("labs_refer.xlsx", id, age, sex, "norm", "no", clsig = "cl", site = "site 02")
-s03_lab <- lab("labs_refer.xlsx", id, age, sex, "norm", "no", clsig = "cl", site = "site 03")
-one_lab <- check(s01_lab, df)
+labs <- lab("labs_refer.xlsx", id, age, sex, "norm", "no")
+one_lab <- check(labs, df)
 
-site_labs <- list(s01_lab, s02_lab, s03_lab)
-site_labs <- check_sites(site_labs, df, site)
-
-test_that("number of incorrect analyzes in different sites", {
-  site_incorrect <- test_sites(site_labs, func = function(x) choose_test(x, "mis"))
-  expect_equal(nrow(site_incorrect), 2)
-})
-
-test_that("number of skipped analyzes in different sites", {
-  site_skip <- test_sites(site_labs, func = function(x) choose_test(x, "skip"))
-  expect_equal(nrow(site_skip), 1)
-})
 
 test_that("number of incorrect analyzes", {
   incorrect <- choose_test(one_lab, "mis")
@@ -66,22 +52,12 @@ test_that("final result is empty", {
   expect_error(check(error_lab, df), "the final result of validation is empty")
 })
 
-test_that("age_min > age_max", {
-  warn_age <- lab("warning_age.xlsx", id, age, sex, "norm", "no")
-  expect_warning(check(warn_age, df), "AGELOW > AGEHIGH in enzyme ast")
-})
-
-test_that("lab_min > lab_max ", {
-  warn_lab <- lab("warning_lab.xlsx", id, age, sex, "norm", "no")
-  expect_warning(check(warn_lab, df), "LBORNRLO > LBORNRHI in enzyme ast")
-})
-
 id <- c("01", "02", "03")
 site <- c("site 01", "site 02", "site 03")
 age <- c(NA, "20", "22")
 sex <- c("f", "m", "f")
 gluc_post <- c(5.5, 4.1, 9.7)
-gluc_res_post <- c("norm", "no", "cl")
+gluc_res_post <- c("norm", "no", "no")
 ast_post <- c("30", "48", "31")
 ast_res_post <- c(NA, "norm", "norm")
 
